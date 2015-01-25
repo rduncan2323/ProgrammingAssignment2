13c0dfe7a3712beb1e8e4a7c36d32e95828b1a09
rduncan2323 <randy.l.duncan@gmail.com>
2015-01-25 09:37:15 -0800

    initial commit of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Project: Coursera programming assignment 2
+## Description: 2 functions
+##  1. makeCacheMatrix: Accepts a matrix as input and makes it accessible to other functions.
+##  2. cacheSolve: accepts the object returned by makeCacheMatrix, checks to determine if the inverted
+##                 matrix has already been cached..if true returns it, else inverts, caches it and then retruns inverted matrix
 
-## Write a short comment describing this function
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmat <- function(mat) m <<- mat
+  getmat <- function() m
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## First try to pull the cached inverted matrix
+        m <- x$getmat()
+        #if it is not null then access the cached matrix else the matrix has "changed" and has not been inverted yet
+        # my assumption is if makeCacheMatrix has been called then the matrix has changed
+        if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+        }
+        #matrix has yet to be cached so get the data
+        data <- x$get()
+        #invert the matrix
+        m <- solve(data)
+        # call setmat to cache the inverted matrix
+        x$setmat(m)
+        m
+        
 }
+
+
+
